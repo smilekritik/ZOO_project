4315cb0c9ae8cb628e15b4aa37e600a0ad15eaa4
kritik <kostia.ponomarenko@gmail.com>
2021-10-05 20:52:28 +0300

    start

---
+++ ZOO_proj/ZOO_proj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZOO_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
